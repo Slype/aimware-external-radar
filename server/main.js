d57e0183a0c2c94c60a828ef57b8e2dd84bc43cb
Slype <likeskippyshow@gmail.com>
2020-01-23 22:23:43 +0100

    fixed variable names and path in server/main.js

--- server/main.js
+++ server/main.js
@@ -3,22 +3,28 @@ const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 const dgram = require('dgram');
 
+// html file directory
+let htmlPath = __dirname.replace(/(\\)/g, "/").split("/").slice(0, -1).join("/") + "/html";
+
 // UDP server
 const udpServer = dgram.createSocket('udp4');
-const udpPort = 10000;
+const udpPort = 10001;
+// SocketIO server
+const socketPort = 10000;
 
 // Listen for when UDP server is up & running
-server.on("listening", () => {
-	console.log(`UDP listening ${server.address().address}:${server.address().port}`);
+udpServer.on("listening", () => {
+    const address = udpServer.address();
+	console.log(`UDP listening ${address.address}:${address.port}`);
 });
 
 // Listen for incomming UDP messages
-server.on("message", (data, rinfo) => {
+udpServer.on("message", (data, rinfo) => {
 
 });
 
 // Start UDP server
-server.bind(udpPort);
+udpServer.bind(udpPort);
 
 // Listen for new socketio connections
 io.on("connection", socket => {
@@ -26,9 +32,9 @@ io.on("connection", socket => {
 });
 
 // Listen for incomming GET requests
-app.get("/*", (req, res) => res.sendFile(__dirname + "/html" + req.url));
+app.get("/*", (req, res) => res.sendFile(htmlPath + req.url));
 
 // Start http server
-http.listen(appPort, () => {
-    console.log("Socket listening on 0.0.0.0:" + appPort);
+http.listen(socketPort, () => {
+    console.log("Socket listening on 0.0.0.0:" + socketPort);
 });
